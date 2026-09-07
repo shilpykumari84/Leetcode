0412c14888ef764dd13d1911574816452af8f0d2
SHILPY <shilpy.kumari.cs27@iilm.edu>
2026-09-07 16:45:14 +0530

    Time: 11 ms (14.43%), Space: 49.4 MB (14.11%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        
+        
+        java.util.Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        
+        java.util.List<int[]> merged = new java.util.ArrayList<>();
+        
+        for (int[] interval : intervals) {
+            
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+        
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
